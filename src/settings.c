a70f40a0b6165f7c408fa124e0bc4a7735a4d13a
Joseph <119084558+DerjenigeUberMensch@users.noreply.github.com>
2024-11-12 08:47:02 +0800

    Reworked setting.(h/c) to use more modular macros

--- src/settings.c
+++ src/settings.c
@@ -13,95 +13,48 @@
 #include "file_util.h"
 
 
-struct 
-USSettingData
+#define VOX_ADD_MEMBER_SETTING(NAME, TYPE, DEFAULT_SETTING) \
+        VOX_ADD_MEMBER(NAME, TYPE, offsetof(UserSettings, NAME), FIELD_SIZEOF(UserSettings, NAME) ,DEFAULT_SETTING)
+
+static const SCSetting
+__USER__SETTINGS__DATA__[] = 
 {
-    const char *const names[UserSettingsLAST];
-    const u8 name_len[UserSettingsLAST];
-    const u8 field_size[UserSettingsLAST];
-    const u16 field_offset[UserSettingsLAST];
-    const enum SCType type[UserSettingsLAST];
-    const Generic default_data[UserSettingsLAST];
-};
-
-#ifdef ADD_MEMBER
-#define ADD_MEMBER1456 ADD_MEMBER
-#undef ADD_MEMBER
-#endif
-
-
-/* non-extension compliant 'switch' statment. */
-#define ADD_MEMBER_SCTypeNoType(DEFAULT_SETTING)     { .data64 = { DEFAULT_SETTING } }
-#define ADD_MEMBER_SCTypeBOOL(DEFAULT_SETTING)       { .data8  = { DEFAULT_SETTING } }
-#define ADD_MEMBER_SCTypeUCHAR(DEFAULT_SETTING)      { .data8  = { DEFAULT_SETTING } }
-#define ADD_MEMBER_SCTypeCHAR(DEFAULT_SETTING)       { .data8i = { DEFAULT_SETTING } }
-#define ADD_MEMBER_SCTypeUSHORT(DEFAULT_SETTING)     { .data16 = { DEFAULT_SETTING } }
-#define ADD_MEMBER_SCTypeSHORT(DEFAULT_SETTING)      { .data16i= { DEFAULT_SETTING } }
-#define ADD_MEMBER_SCTypeUINT(DEFAULT_SETTING)       { .data32 = { DEFAULT_SETTING } }
-#define ADD_MEMBER_SCTypeINT(DEFAULT_SETTING)        { .data32i= { DEFAULT_SETTING } }
-#define ADD_MEMBER_SCTypeFLOAT(DEFAULT_SETTING)      { .dataf  = { DEFAULT_SETTING } }
-#define ADD_MEMBER_SCTypeDOUBLE(DEFAULT_SETTING)     { .datad  = { DEFAULT_SETTING } }
-#define ADD_MEMBER_SCTypeLONG(DEFAULT_SETTING)       { .data64i= { DEFAULT_SETTING } }
-#define ADD_MEMBER_SCTypeULONG(DEFAULT_SETTING)      { .data64 = { DEFAULT_SETTING } }
-#define ADD_MEMBER_SCTypeSTRING(DEFAULT_SETTING)     { .v      = { DEFAULT_SETTING } }
-
-#define ADD_MEMBER_TYPED43(TYPE, DEFAULT_SETTING) \
-        ADD_MEMBER_##TYPE(DEFAULT_SETTING)
-
-#define ADD_MEMBER(NAME, STRUCT_NAME, TYPE, DEFAULT_SETTING) \
-        .names[NAME] = #NAME, \
-        .name_len[NAME] = sizeof(#NAME), \
-        .field_size[NAME] = FIELD_SIZEOF(UserSettings, STRUCT_NAME), \
-        .field_offset[NAME] = offsetof(UserSettings, STRUCT_NAME), \
-        .type[NAME] = TYPE, \
-        .default_data[NAME] = ADD_MEMBER_TYPED43(TYPE, DEFAULT_SETTING),
-
-static const struct USSettingData
-__USER__SETTINGS__DATA__ = 
-{
-    ADD_MEMBER(MFact, mfact, SCTypeFLOAT, 0.55f)
-    ADD_MEMBER(GapRatio, gapratio, SCTypeFLOAT, 0.95f)
-    ADD_MEMBER(MCount, mcount, SCTypeUSHORT, 1)
-    ADD_MEMBER(Snap, snap, SCTypeUSHORT, 10)
-    ADD_MEMBER(RefreshRate, refreshrate, SCTypeUSHORT, 60)
-    ADD_MEMBER(MaxCC, maxcc, SCTypeUSHORT, 256)
+    VOX_ADD_MEMBER_SETTING(MFact, SCTypeFLOAT, 0.55f)
+    VOX_ADD_MEMBER_SETTING(GapRatio, SCTypeFLOAT, 0.95f)
+    VOX_ADD_MEMBER_SETTING(MCount, SCTypeUSHORT, 1)
+    VOX_ADD_MEMBER_SETTING(Snap, SCTypeUSHORT, 10)
+    VOX_ADD_MEMBER_SETTING(RefreshRate, SCTypeUSHORT, 60)
+    VOX_ADD_MEMBER_SETTING(MaxCC, SCTypeUSHORT, 256)
 
     /* BOOL Types */
-    ADD_MEMBER(HoverFocus, hoverfocus, SCTypeBOOL, false)
-    ADD_MEMBER(UseDecorations, usedecorations, SCTypeBOOL, false)
-    ADD_MEMBER(UseClientSideDecorations, useclientdecorations, SCTypeBOOL, true)
-    ADD_MEMBER(PreferClientSideDecorations, preferclientdecorations, SCTypeBOOL, true)
+    VOX_ADD_MEMBER_SETTING(HoverFocus, SCTypeBOOL, false)
+    VOX_ADD_MEMBER_SETTING(UseDecorations, SCTypeBOOL, false)
+    VOX_ADD_MEMBER_SETTING(UseClientSideDecorations, SCTypeBOOL, true)
+    VOX_ADD_MEMBER_SETTING(PreferClientSideDecorations, SCTypeBOOL, true)
 
 
     /* bar data */
-    ADD_MEMBER(BarLX, lx, SCTypeFLOAT, 0.0f)    /*   lx    */
-    ADD_MEMBER(BarLY, ly, SCTypeFLOAT, 0.0f)    /*   ly    */
-    ADD_MEMBER(BarLW, lw, SCTypeFLOAT, 0.15f)   /*   lw    */
-    ADD_MEMBER(BarLH, lh, SCTypeFLOAT, 1.0f)    /*   lh    */
+    VOX_ADD_MEMBER_SETTING(BarLX, SCTypeFLOAT, 0.0f)    /*   lx    */
+    VOX_ADD_MEMBER_SETTING(BarLY, SCTypeFLOAT, 0.0f)    /*   ly    */
+    VOX_ADD_MEMBER_SETTING(BarLW, SCTypeFLOAT, 0.15f)   /*   lw    */
+    VOX_ADD_MEMBER_SETTING(BarLH, SCTypeFLOAT, 1.0f)    /*   lh    */
 
-    ADD_MEMBER(BarRX, rx, SCTypeFLOAT, 0.85f)   /* rx - rw */
-    ADD_MEMBER(BarRY, ry, SCTypeFLOAT, 0.0f)    /*   ry    */
-    ADD_MEMBER(BarRW, rw, SCTypeFLOAT, 0.15f)   /*   rw    */
-    ADD_MEMBER(BarRH, rh, SCTypeFLOAT, 1.0f)    /*   rh    */
+    VOX_ADD_MEMBER_SETTING(BarRX, SCTypeFLOAT, 0.85f)   /* rx - rw */
+    VOX_ADD_MEMBER_SETTING(BarRY, SCTypeFLOAT, 0.0f)    /*   ry    */
+    VOX_ADD_MEMBER_SETTING(BarRW, SCTypeFLOAT, 0.15f)   /*   rw    */
+    VOX_ADD_MEMBER_SETTING(BarRH, SCTypeFLOAT, 1.0f)    /*   rh    */
 
-    ADD_MEMBER(BarTX, tx, SCTypeFLOAT, 0.0f)    /*   tx    */
-    ADD_MEMBER(BarTY, ty, SCTypeFLOAT, 0.0f)    /*   ty    */
-    ADD_MEMBER(BarTW, tw, SCTypeFLOAT, 1.0f)    /*   tw    */
-    ADD_MEMBER(BarTH, th, SCTypeFLOAT, 0.15f)   /*   th    */
+    VOX_ADD_MEMBER_SETTING(BarTX, SCTypeFLOAT, 0.0f)    /*   tx    */
+    VOX_ADD_MEMBER_SETTING(BarTY, SCTypeFLOAT, 0.0f)    /*   ty    */
+    VOX_ADD_MEMBER_SETTING(BarTW, SCTypeFLOAT, 1.0f)    /*   tw    */
+    VOX_ADD_MEMBER_SETTING(BarTH, SCTypeFLOAT, 0.15f)   /*   th    */
 
-    ADD_MEMBER(BarBX, bx, SCTypeFLOAT, 0.0f)    /*   bx    */
-    ADD_MEMBER(BarBY, by, SCTypeFLOAT, 0.85f)   /* bx - bh */
-    ADD_MEMBER(BarBW, bw, SCTypeFLOAT, 1.0f)    /*   bw    */
-    ADD_MEMBER(BarBH, bh, SCTypeFLOAT, 0.15f)   /*   bh    */
+    VOX_ADD_MEMBER_SETTING(BarBX, SCTypeFLOAT, 0.0f)    /*   bx    */
+    VOX_ADD_MEMBER_SETTING(BarBY, SCTypeFLOAT, 0.85f)   /* bx - bh */
+    VOX_ADD_MEMBER_SETTING(BarBW, SCTypeFLOAT, 1.0f)    /*   bw    */
+    VOX_ADD_MEMBER_SETTING(BarBH, SCTypeFLOAT, 0.15f)   /*   bh    */
 };
 
-
-#undef ADD_MEMBER
-
-#ifdef ADD_MEMBER1456
-#define ADD_MEMBER ADD_MEMBER1456
-#undef ADD_MEMBER1456
-#endif
 
 void
 USSetupCFGVars(
@@ -113,18 +66,19 @@ USSetupCFGVars(
     }
 
     SCParser *cfg = us->cfg;
+    const SCSetting *usdata = __USER__SETTINGS__DATA__;
 
     i32 i;
     u8 err = 0;
     const int READONLY = 1;
-    const struct USSettingData *const usdata = &__USER__SETTINGS__DATA__;
     /* global settings */
     for(i = 0; i < UserSettingsLAST; ++i)
     {   
-        err = SCParserNewVar(cfg, usdata->names[i], usdata->name_len[i], READONLY, usdata->field_size[i], usdata->type[i]);
+        err = SCParserNewVar(cfg, usdata->name, usdata->name_len, READONLY, usdata->size, usdata->type);
         if(err)
-        {   Debug("Failed to create: \"%s\"", usdata->names[i]);
+        {   Debug("Failed to create: \"%s\"", usdata->name);
         }
+        ++usdata;
     }
 }
 
@@ -140,11 +94,12 @@ USSetupCFGDefaults(
     UserSettings *s = us;
     void *data;
     i32 i;
-    const struct USSettingData *const usdata = &__USER__SETTINGS__DATA__;
+    const SCSetting *usdata = __USER__SETTINGS__DATA__;
     for(i = 0; i < UserSettingsLAST; ++i)
     {
-        data = ((uint8_t *)s) + usdata->field_offset[i];
-        memcpy(data, &usdata->default_data[i], usdata->field_size[i]);
+        data = ((uint8_t *)s) + usdata->offset;
+        memcpy(data, &usdata->default_data, usdata->size);
+        ++usdata;
     }
 }
 
@@ -153,9 +108,6 @@ USInit(
         UserSettings *settings_init
         )
 {
-    if(!settings_init)
-    {   return;
-    }
     memset(settings_init, 0, sizeof(UserSettings));
     settings_init->cfg = SCParserCreate(UserSettingsLAST);
     if(settings_init->cfg)
@@ -171,7 +123,7 @@ USLoad(
         UserSettings *settings
         )
 {
-    if(!settings || !settings->cfg)
+    if(!settings->cfg)
     {   return;
     }
 
@@ -195,24 +147,26 @@ USLoad(
     }
     i32 i;
     void *data;
-    const struct USSettingData *const usdata = &__USER__SETTINGS__DATA__;
+
+    const SCSetting *usdata = __USER__SETTINGS__DATA__;
     for(i = 0; i < UserSettingsLAST; ++i)
     {
-        data = ((uint8_t *)settings) + usdata->field_offset[i];
-        item = SCParserSearch(cfg, usdata->names[i]);
+        data = ((uint8_t *)settings) + usdata->offset;
+        item = SCParserSearch(cfg, usdata->name);
         if(!item)
-        {   item = SCParserSearchSlow(cfg, usdata->names[i]);
+        {   item = SCParserSearchSlow(cfg, usdata->name);
         }
         if(item)
         {   
-            status = SCParserLoad(item, data, usdata->field_size[i], usdata->type[i]);
+            status = SCParserLoad(item, data, usdata->size, usdata->type);
             if(status)
-            {   Debug("Failed to LOAD, \"%s\"", usdata->names[i]);
+            {   Debug("Failed to LOAD, \"%s\"", usdata->name);
             }
         }
         else
-        {   Debug("Failed to FIND, \"%s\"", usdata->names[i]);
+        {   Debug("Failed to FIND, \"%s\"", usdata->name);
         }
+        ++usdata;
     }
 }
 
@@ -221,16 +175,18 @@ USSave(
         UserSettings *settings
         )
 {
-    if(!settings || !settings->cfg)
+    if(!settings->cfg)
     {   return;
     }
     SCParser *cfg = settings->cfg;
     UserSettings *s = settings;
     i32 i;
-    const struct USSettingData *const usdata = &__USER__SETTINGS__DATA__;
+    const SCSetting *usdata = __USER__SETTINGS__DATA__;
 
     for(i = 0; i < UserSettingsLAST; ++i)
-    {   SCParserSaveVar(cfg, usdata->names[i], ((uint8_t *)s) + usdata->field_offset[i]);
+    {   
+        SCParserSaveVar(cfg, usdata->name, ((uint8_t *)s) + usdata->offset);
+        ++usdata;
     }
 
     char __CONFIG__PATH__[FFSysGetConfigPathLengthMAX];
@@ -266,7 +222,7 @@ USWipe(
         UserSettings *settings
         )
 {
-    if(!settings || !settings->cfg)
+    if(!settings->cfg)
     {   return;
     }
     SCParser *cfg = settings->cfg;
