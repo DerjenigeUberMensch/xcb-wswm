0df2bc9f5e242f1909b7f022f302cd8ae4cb30d4
Joseph <119084558+DerjenigeUberMensch@users.noreply.github.com>
2024-12-06 07:21:11 +0800

    Updated TPromise Library

--- tools/TPromises/tpromise.h
+++ tools/TPromises/tpromise.h
@@ -1,3 +1,26 @@
+/* MIT License
+ *
+ * Copyright (c) 2024 Joseph
+ * All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 #ifndef __THREAD__PROMISE__H__
 #define __THREAD__PROMISE__H__
 
@@ -37,12 +60,24 @@ AwaitTPromise(
 /* Destroys the given promise, usage of promise after being destroyed is undefined.
  * 
  * NOTE: if a promise is being resolved or awaited, behaviour is undefined.
+ * NOTE: Calls 'free' on promise. -> free(promise);
  *
  */
 void
 DestroyTPromise(
     TPromise *promise
     );
+
+/* Destroys the given promise, usage of promise after being destroyed is undefined.
+ * 
+ * NOTE: if a promise is being resolved or awaited, behaviour is undefined.
+ * NOTE: Does not call 'free' on promise. 
+ *
+ */
+void
+DestroyTPromiseFilled(
+        TPromise *promise
+        );
 
 /* Returns wheter or not promise has resolved its data.
  *
